d3e8d8737f1f7c130d328f7f458be3c30a3c2928
junaeid <junaeid.md48@gmail.com>
2024-05-22 10:32:15 +0600

    Added: Code comments.

--- src/components/Alphabate_Tile.jsx
+++ src/components/Alphabate_Tile.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
 import "./style.css"
+
+// Alphabets list.
 const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 function Alphabate_Tile() {
-  const [result, setResult] = useState("")
-  const [repeatVal, setRepeatVal] = useState(0)
+  const [result, setResult] = useState("") // Result state
+  const [repeatVal, setRepeatVal] = useState(0) // Traking number of repeated values.
+
+  // Each alphabets click hanndler function.
   const handleClick = (value) => {
-    if (result != "" && value === result[result.length - 1]) {
+    if (result != "" && value === result[result.length - 1]) { // check if the result is not empty and char is repeated
+      // char repeated meaning. There is two same char in the result.
       if (repeatVal < 1) {
         setResult(result + value)
         setRepeatVal(repeatVal + 1)
       } else {
+        // Replace repeated chars with "_".
         var tempString = result.substring(0, result.length - 2) + "_"
         setResult(tempString)
         setRepeatVal(0)
@@ -21,7 +27,7 @@ function Alphabate_Tile() {
     }
   }
 
-
+  // Resets the app state.
   const handleReset = () => {
     setResult("");
     setRepeatVal(0);
